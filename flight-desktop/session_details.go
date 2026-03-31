c07798f97f1bc22b19cea0fc52128613c06eee21
James Muscat <james.muscat@alces-software.com>
2026-03-31 16:03:13 +0100

    Fix typo.

--- flight-desktop/session_details.go
+++ flight-desktop/session_details.go
@@ -27,7 +27,7 @@ func printSessionStartedMessage(session Session) {
 	if session.Name != "" {
 		name = fmt.Sprintf("(%s) ", session.Name)
 	}
-	out := fmt.Sprintf("A new destop session %shas been started. To connect, use the details below:\n\n1. Connection Address\n\nCopy and paste the first address into your VNC viewer. If that doesn't work, try one of the alternatives:\n\n* Primary:       %s\n* Alternative 1: %s\n* Alternative 2: %s\n\n2. Password\n\nIf prompted, use this password: %s\n\n3. Need Help?\n\nFor a more details on how to connect, run: 'flight howto show flight-desktop'\n\n4. Manage this session\n\nTo view details or stop this session later, you will need the Session ID:\n\n    %s\n\n(Tip: Run 'flight desktop --help' to see management commands)\n\nStarted at: %s\n", name, ipPort, vnc, ipDisplay, session.Password, session.ID, session.CreatedAt.Format(time.RFC822))
+	out := fmt.Sprintf("A new desktop session %shas been started. To connect, use the details below:\n\n1. Connection Address\n\nCopy and paste the first address into your VNC viewer. If that doesn't work, try one of the alternatives:\n\n* Primary:       %s\n* Alternative 1: %s\n* Alternative 2: %s\n\n2. Password\n\nIf prompted, use this password: %s\n\n3. Need Help?\n\nFor a more details on how to connect, run: 'flight howto show flight-desktop'\n\n4. Manage this session\n\nTo view details or stop this session later, you will need the Session ID:\n\n    %s\n\n(Tip: Run 'flight desktop --help' to see management commands)\n\nStarted at: %s\n", name, ipPort, vnc, ipDisplay, session.Password, session.ID, session.CreatedAt.Format(time.RFC822))
 	out = wordwrap.String(out, 80)
 	fmt.Print(out)
 }
