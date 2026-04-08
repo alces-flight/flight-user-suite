dc9611a2776c11c4c568a1897254662fd6c4fd6b
megan.godfrey <megan.godfrey@alces-software.com>
2026-04-08 17:45:32 +0100

    Display hardcoded markdown file in one view

--- flight-howto/tui.go
+++ flight-howto/tui.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/fs"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
 	"charm.land/bubbles/v2/spinner"
-	"charm.land/bubbles/v2/timer"
+	"charm.land/bubbles/v2/viewport"
 	tea "charm.land/bubbletea/v2"
+	"charm.land/glamour/v2"
+	"charm.land/glamour/v2/ansi"
 	"charm.land/lipgloss/v2"
+	"github.com/concertim/flight-user-suite/flight/pkg"
 )
 
 // sessionState is used to track which model is focused
@@ -35,36 +43,123 @@ var (
 		spinner.Monkey,
 	}
 	modelStyle = lipgloss.NewStyle().
-			Width(15).
-			Height(5).
+			Width(40).
+			Height(10).
 			Align(lipgloss.Center, lipgloss.Center).
 			BorderStyle(lipgloss.HiddenBorder())
 	focusedModelStyle = lipgloss.NewStyle().
-				Width(15).
-				Height(5).
+				Width(40).
+				Height(10).
 				Align(lipgloss.Center, lipgloss.Center).
 				BorderStyle(lipgloss.NormalBorder()).
-				BorderForeground(lipgloss.Color("69"))
-	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("69"))
-	helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
+				BorderForeground(pkg.LightBlue)
+	spinnerStyle = lipgloss.NewStyle().Foreground(pkg.AlcesBlue)
+	helpStyle    = lipgloss.NewStyle().Foreground(pkg.Grey)
 )
 
 type mainModel struct {
 	state   sessionState
-	timer   timer.Model
+	guide   example
 	spinner spinner.Model
 	index   int
 }
 
-func newModel(timeout time.Duration) mainModel {
+type example struct {
+	viewport viewport.Model
+}
+
+func newExample(isDark bool) (*example, error) {
+	const (
+		width  = 38
+		height = 10
+	)
+
+	vp := viewport.New()
+	vp.SetWidth(width)
+	vp.SetHeight(height)
+	vp.Style = lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(pkg.AlcesBlue)
+
+	// We need to adjust the width of the glamour render from our main width
+	// to account for a few things:
+	//
+	//  * The viewport border width
+	//  * The viewport padding
+	//  * The viewport margins
+	//  * The gutter glamour applies to the left side of the content
+	//
+	const glamourGutter = 3
+	glamourRenderWidth := width - vp.Style.GetHorizontalFrameSize() - glamourGutter
+
+	markdownThemeFilename := filepath.Join(markdownThemeDir, "flight-light.json")
+	if isDark {
+		markdownThemeFilename = filepath.Join(markdownThemeDir, "flight-dark.json")
+	}
+	markdownThemeBytes, err := os.ReadFile(markdownThemeFilename)
+	if err != nil {
+		return nil, err
+	}
+	var style ansi.StyleConfig
+	if err := json.Unmarshal(markdownThemeBytes, &style); err != nil {
+		return nil, fmt.Errorf("parsing markdown theme: %w", err)
+	}
+	renderer, err := glamour.NewTermRenderer(
+		glamour.WithStyles(style),
+		glamour.WithWordWrap(glamourRenderWidth),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	markdown, err := markdownContent()
+	if err != nil {
+		return nil, err
+	}
+	str, err := renderer.Render(string(markdown))
+	if err != nil {
+		return nil, err
+	}
+
+	vp.SetContent(str)
+
+	return &example{
+		viewport: vp,
+	}, nil
+}
+
+func markdownContent() ([]byte, error) {
+	filenames, err := collectMarkdownFiles(howtoDir)
+	if err != nil {
+		return nil, fmt.Errorf("collecting guide files: %w", err)
+	}
+	howtoName := filenames[1]
+	fullPath := filepath.Join(howtoDir, howtoName)
+	if !strings.HasSuffix(fullPath, ".md") {
+		fullPath = fullPath + ".md"
+	}
+	markdown, err := os.ReadFile(fullPath)
+	if err != nil {
+		if pathError, ok := errors.AsType[*fs.PathError](err); ok {
+			if pathError.Err.Error() == "no such file or directory" {
+				return nil, UnknownHowto{Howto: howtoName}
+			}
+		}
+		return nil, fmt.Errorf("reading howto: %w", err)
+	}
+	return markdown, nil
+}
+
+func newModel() (mainModel, error) {
 	m := mainModel{state: timerView}
-	m.timer = timer.New(timeout)
+	guide, err := newExample(lipgloss.HasDarkBackground(os.Stdin, os.Stdout))
+	m.guide = *guide
 	m.spinner = spinner.New()
-	return m
+	return m, err
 }
 
 func (m mainModel) Init() tea.Cmd {
-	return tea.Batch(m.timer.Init(), m.spinner.Tick)
+	return m.spinner.Tick
 }
 
 func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -82,10 +177,7 @@ func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.state = timerView
 			}
 		case "n":
-			if m.state == timerView {
-				m.timer = timer.New(defaultTime)
-				cmds = append(cmds, m.timer.Init())
-			} else {
+			if m.state == spinnerView {
 				m.Next()
 				m.resetSpinner()
 				cmds = append(cmds, m.spinner.Tick)
@@ -97,14 +189,11 @@ func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.spinner, cmd = m.spinner.Update(msg)
 			cmds = append(cmds, cmd)
 		default:
-			m.timer, cmd = m.timer.Update(msg)
+			m.guide.viewport, cmd = m.guide.viewport.Update(msg)
 			cmds = append(cmds, cmd)
 		}
 	case spinner.TickMsg:
 		m.spinner, cmd = m.spinner.Update(msg)
-		cmds = append(cmds, cmd)
-	case timer.TickMsg:
-		m.timer, cmd = m.timer.Update(msg)
 		cmds = append(cmds, cmd)
 	}
 	return m, tea.Batch(cmds...)
@@ -114,9 +203,9 @@ func (m mainModel) View() tea.View {
 	var s strings.Builder
 	model := m.currentFocusedModel()
 	if m.state == timerView {
-		s.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, focusedModelStyle.Render(fmt.Sprintf("%4s", m.timer.View())), modelStyle.Render(m.spinner.View())))
+		s.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, focusedModelStyle.Render(fmt.Sprintf("%4s", m.guide.viewport.View())), modelStyle.Render(m.spinner.View())))
 	} else {
-		s.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, modelStyle.Render(fmt.Sprintf("%4s", m.timer.View())), focusedModelStyle.Render(m.spinner.View())))
+		s.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, modelStyle.Render(fmt.Sprintf("%4s", m.guide.viewport.View())), focusedModelStyle.Render(m.spinner.View())))
 	}
 	s.WriteString(helpStyle.Render(fmt.Sprintf("\ntab: focus next • n: new %s • q: exit\n", model)))
 	return tea.NewView(s.String())
@@ -144,9 +233,13 @@ func (m *mainModel) resetSpinner() {
 }
 
 func tui() {
-	p := tea.NewProgram(newModel(defaultTime))
-
-	if _, err := p.Run(); err != nil {
+	var p *tea.Program
+	if m, err := newModel(); err == nil {
+		p = tea.NewProgram(m)
+		if _, err := p.Run(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
 		log.Fatal(err)
 	}
 }
