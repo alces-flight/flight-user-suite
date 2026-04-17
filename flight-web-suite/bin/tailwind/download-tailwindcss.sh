055ff4e8b52ca00ef07cc8f433c2c849bb3e8b90
James Muscat <james.muscat@alces-software.com>
2026-04-17 12:16:40 +0100

    Download Tailwind to expected location.

--- flight-web-suite/bin/tailwind/download-tailwindcss.sh
+++ flight-web-suite/bin/tailwind/download-tailwindcss.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # file name
 TAILWIND_BIN="tailwindcss"
-TAILWIND_BIN_PATH="${PWD}/${TAILWIND_BIN}"
+TAILWIND_BIN_PATH="${SCRIPT_DIR}/${TAILWIND_BIN}"
 
-echo "Downloading Tailwind CSS CLI..."
+echo "Downloading Tailwind CSS CLI to ${TAILWIND_BIN_PATH}..."
 
 # OS detector
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  curl -L https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64 -o "$TAILWIND_BIN"
+  curl -sSL https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64 -o "$TAILWIND_BIN_PATH"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  curl -L https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-macos-arm64 -o "$TAILWIND_BIN"
+  curl -sSL https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-macos-arm64 -o "$TAILWIND_BIN_PATH"
 else
   echo "OS not supported. Try to download manually"
   exit 1
@@ -18,7 +19,6 @@ fi
 
 # Set permission to make the file executable
 echo "Setting the file as executable..."
-echo "$PWD"
 chmod +x "$TAILWIND_BIN_PATH"
 
 echo "Tailwind CSS CLI is Installed!"
