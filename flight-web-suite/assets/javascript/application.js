fdf3786aabd68c8532c180f6ce319c31926015c3
megan.godfrey <megan.godfrey@alces-software.com>
2026-04-29 12:39:02 +0100

    Move dropdown js to specialised file

--- flight-web-suite/assets/javascript/application.js
+++ flight-web-suite/assets/javascript/application.js
@@ -1,6 +1,1 @@
-const dropdownToggles = document.getElementsByClassName('dropdown-toggle');
-for (const dropdownToggle of dropdownToggles) {
-    dropdownToggle.addEventListener("click", (e) => {
-        document.getElementById(dropdownToggle.dataset.targetId).classList.toggle("hidden");
-    })
-}
+import "./dropdown";
