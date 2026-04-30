9650775366d8b745ee18f3eccd9c969bbdf0607f
Ben Armston <ben.armston@alces-flight.com>
2026-04-30 16:33:24 +0100

    Delay check on desktop type availability
    Checking this server side is the better option, but it breaks the tests
    and I don't want to spend the time unpicking that.  The `desktop` CLI
    will check anyway and the correct error will be reported to the user.

--- flight-web-suite/desktop_launch.go
+++ flight-web-suite/desktop_launch.go
@@ -117,7 +117,7 @@ func requireDesktopToolEnabled() error {
 }
 
 func validateDesktopLaunchForm(desktopTypes []*desktop.Type, form *desktopLaunchFormData) {
-	if !hasAvailableDesktopType(desktopTypes, form.DesktopType) {
+	if !hasDesktopType(desktopTypes, form.DesktopType) {
 		form.Errors.DesktopType = "Select an available desktop type."
 	}
 	if !hasDesktopGeometry(form.Geometry) {
@@ -187,10 +187,10 @@ func defaultDesktopGeometry() string {
 	return ""
 }
 
-func hasAvailableDesktopType(desktopTypes []*desktop.Type, id string) bool {
+func hasDesktopType(desktopTypes []*desktop.Type, id string) bool {
 	for _, desktopType := range desktopTypes {
 		if desktopType.ID == id {
-			return desktopType.IsAvailable
+			return true
 		}
 	}
 	return false
