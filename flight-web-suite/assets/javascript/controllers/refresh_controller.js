947e1e5256414c05911826ad9c3b07a5c1e9d083
Ben Armston <ben.armston@alces-flight.com>
2026-05-06 11:45:54 +0100

    Add cache buster to screenshots on page load
    Previously, the cache buster was only added once the screenshot was
    refreshed after 5 minutes.  Until then it would potentially use a cached
    value which may well have been the placeholder image.

--- flight-web-suite/assets/javascript/controllers/refresh_controller.js
+++ flight-web-suite/assets/javascript/controllers/refresh_controller.js
@@ -4,19 +4,33 @@ import { Controller } from "@hotwired/stimulus"
 //
 // Connects to data-controller="refresh"
 export default class extends Controller {
-  static values = { interval: { type: Number, default: 60 * 1000 } }
+  static values = {
+    interval: { type: Number, default: 60 * 1000 },
+    src: String,
+  }
+
+  initialize() {
+    this.refresh = this.refresh.bind(this)
+  }
 
   connect() {
-    this.initialSrc = this.element.src
-    this.intervalId = setInterval(this.refresh.bind(this), this.intervalValue)
+    this.intervalId = setInterval(this.refresh, this.intervalValue)
   }
 
   disconnect() {
     clearInterval(this.intervalId)
   }
 
+  srcValueChanged() {
+    const initial = this.element.src == null || this.element.src == ""
+    if (initial && this.srcValue != null) {
+      this.element.src = this.srcValue
+    }
+    setTimeout(this.refresh, 250)
+  }
+
   refresh() {
     const cacheBuster = Date.now()
-    this.element.src = `${this.initialSrc}?t=${cacheBuster}`
+    this.element.src = `${this.srcValue}?t=${cacheBuster}`
   }
 }
